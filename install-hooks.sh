98692e911cb89f8c9845f5ca57d8e97e1874ff92
DavidK <mrkoubek@gmail.com>
2024-10-13 20:58:43 +0200

    Hooks - post-commit - improve logging code to amend the commit with the log file

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This script copies hooks from the 'hooks/' folder to the '.git/hooks/' folder.
+# To launch the script, open Git Bash, and type in:
+    # bash install-hooks.sh
 
 # Copy each hook to the .git/hooks/ directory.
 cp -r "./hooks/"* ".git/hooks/"
